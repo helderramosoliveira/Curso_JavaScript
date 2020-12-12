a7f977e84eb3c329eb31ff3ea1fd0a185ebc6ade
helderramosoliveira <helder.ramos.oliveira@gmail.com>
2020-12-12 12:19:36 -0300

    Condicional js - aula 11

---
+++ Aula11/ex008.js
@@ -0,0 +1,9 @@
+//console.log('O console funcionou corretamente')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitira`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
