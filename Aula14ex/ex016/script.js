b300c5ac12989c4cb95bcc44c9f789bebb93f995
helderramosoliveira <helder.ramos.oliveira@gmail.com>
2020-12-13 15:51:18 -0300

    Adicionando alguns comentarios importantes e modificações pontuais do aprendizado

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -5,23 +5,25 @@ function contar(){
     var txt_res = '' 
     var t_res = document.getElementById('div_res')
 
-    if ( n_ini == '' || n_fim == '' ) {
+    if ( n_ini == '' || n_fim == '' ) { //value.length == 0  também funciona
         t_res.innerHTML = 'Faltando Dados. Impossível contar'
     } else if (n_pas == 0) {
         t_res.innerHTML = 'Passo não pode ser zero!'
     } else {
         if (n_fim >= n_ini) {
-            for(n_ini ; n_ini <= n_fim; n_ini += n_pas){
-                txt_res += n_ini + ' &#128073 '
+            //contagem crescente
+            for(n_ini ; n_ini <= n_fim; n_ini += n_pas){  // seria melhor usar um indice (i = n_ini; i <= n_fim; i += n_pas) se eu tentar pegar o valor de n_ini de novo, ferrou!
+                t_res.innerHTML += n_ini + ` \u{1F449} ` //' &#128073 ' // unicode para js \u{1F603}
             }
         
-            t_res.innerHTML = txt_res + ' &#127987 '
+            t_res.innerHTML += ' &#127987 '
         } else { 
+            //contagem regressiva
             for(n_ini ; n_ini >= n_fim; n_ini -= n_pas){
-                txt_res += n_ini + ' &#128073 '
+                t_res.innerHTML += n_ini + ' &#128073 '
             }
         
-            t_res.innerHTML = txt_res + ' &#127987 '
+            t_res.innerHTML +=  ' &#127987 '
         }
 
     }
--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -2,25 +2,29 @@ function calcular(){
     var num = document.getElementById('number').value
     var sel = document.getElementById('res')
 
-    for (i=0; i<=9; i++){
-        sel.remove('opt_res')
-    }    
+    sel.innerHTML = ''
+//    for (i=0; i<=9; i++){          // NAO PRECISA FAZER ISSO
+//        sel.remove('opt_res')
+//    }    
 
     if (num == ''){
         alert('o Número não pode ser nulo')
+        
+        //voltando ao padrao
         var opt = document.createElement('option')
         opt.setAttribute('id', 'opt_res')
         opt.innerHTML = 'Digite um numero acima'
         sel.add(opt)
+
     } else {
         for (var i = 0; i <= 9; i++) {
-            var soma = Number(num) * i
+            var soma = Number(num) * i 
             
             var opt = document.createElement('option')
             opt.setAttribute('id', 'opt_res')
             opt.setAttribute('value', i )
 
-            opt.innerHTML = `${num} x ${i} = <strong>${soma}</strong>`
+            opt.innerHTML = `${num} x ${i} = <strong>${soma}</strong>` //aqui em vez de soma eu poderia usar ${num*i}
 
            sel.add(opt)
         }
