3e54b1a4cc91ef1d092c148b337cfed754144fe8
helderramosoliveira <helder.ramos.oliveira@gmail.com>
2020-12-12 23:49:58 -0300

    Finalizando laços Aulas 13 e 14 sem exercicios

---
+++ Aula13/ambiente.js
@@ -0,0 +1,19 @@
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
+
+var c = 1
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+    console.log('----------------------------------------')
+
+c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 3)
